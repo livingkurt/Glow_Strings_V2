cf64d27c5413569efce70b52b0860a064f3f2328
Kurt LaVacque <lavacquek@icloud.com>
2020-12-11 22:59:28 -0600

    Got the Mirror Shooting Star ones working

--- Shooting_Star.ino
+++ Shooting_Star.ino
@@ -19,17 +19,42 @@
 //   leds[(pos1 + pos2) / 2] += CHSV(255, 0, 255);
 // }
 
+// void shooting_star_white()
+// {
+//   int rate = 10;
+//   int start_hue;
+//   int delta_hue = 30;
+//   start_hue = -1 * millis() / rate;
+//   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
+//   uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
+//   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
+//   leds[pos_1] = CHSV(start_hue, 0, 255);
+//   FastLED.show();
+//   start_hue += delta_hue;
+// }
+
 void shooting_star_white()
 {
   int rate = 10;
   int start_hue;
   int delta_hue = 30;
+  static int last_pos = 0;
   start_hue = -1 * millis() / rate;
   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
   uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
-  uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
-  leds[pos_1] = CHSV(start_hue, 0, 255);
-  FastLED.show();
+  uint16_t pos = map(u, 0, 255, 0, NUM_LEDS - 1);
+  while (last_pos != pos)
+  {
+    if (last_pos < pos)
+    {
+      last_pos++;
+    }
+    else
+    {
+      last_pos = 0;
+    }
+    leds[last_pos] += CHSV(255, 0, 255);
+  }
   start_hue += delta_hue;
 }
 
@@ -76,17 +101,42 @@ void shooting_star_white()
 //   leds[beat8(50, 0)] |= CHSV(255, 0, 255);
 // }
 
+// void shooting_star_rainbow()
+// {
+//   int rate = 10;
+//   int start_hue;
+//   int delta_hue = 30;
+//   start_hue = -1 * millis() / rate;
+//   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
+//   uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
+//   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
+//   leds[pos_1] = CHSV(start_hue, 255, 255);
+//   FastLED.show();
+//   start_hue += delta_hue;
+// }
+
 void shooting_star_rainbow()
 {
   int rate = 10;
   int start_hue;
   int delta_hue = 30;
+  static int last_pos = 0;
   start_hue = -1 * millis() / rate;
   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
   uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
-  uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
-  leds[pos_1] = CHSV(start_hue, 255, 255);
-  FastLED.show();
+  uint16_t pos = map(u, 0, 255, 0, NUM_LEDS - 1);
+  while (last_pos != pos)
+  {
+    if (last_pos < pos)
+    {
+      last_pos++;
+    }
+    else
+    {
+      last_pos = 0;
+    }
+    leds[last_pos] += CHSV(start_hue, 255, 255);
+  }
   start_hue += delta_hue;
 }
 
@@ -312,39 +362,111 @@ int determine_speed_shooting_star_bnf()
 
 // =================== Shooting Star White Mirror ===================
 
+// void shooting_star_white_mirror()
+// {
+//   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star_mirror()); //change fadeby to smaller or larger number to adjust the length of the tail.
+//   uint8_t u;
+//   u = beat8(determine_speed_shooting_star_mirror(), 0); //BPM will allow you to adjust the speed the dot is moving.
+//   // Forward
+//   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
+//   // Backward
+//   uint16_t pos_2 = map(u, 0, 255, NUM_LEDS - 1, 0);
+//   leds[pos_1] = CRGB::White;
+//   leds[pos_2] = CRGB::White;
+//   FastLED.show();
+// }
+
 void shooting_star_white_mirror()
 {
-  fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star_mirror()); //change fadeby to smaller or larger number to adjust the length of the tail.
-  uint8_t u;
-  u = beat8(determine_speed_shooting_star_mirror(), 0); //BPM will allow you to adjust the speed the dot is moving.
-  // Forward
+  int rate = 10;
+  int start_hue;
+  int delta_hue = 30;
+  static int last_pos_1 = 0;
+  static int last_pos_2 = 0;
+  start_hue = -1 * millis() / rate;
+  fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
+  uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
-  // Backward
   uint16_t pos_2 = map(u, 0, 255, NUM_LEDS - 1, 0);
-  leds[pos_1] = CRGB::White;
-  leds[pos_2] = CRGB::White;
-  FastLED.show();
+  while (last_pos_1 != pos_1)
+  {
+    if (last_pos_1 < pos_1)
+    {
+      last_pos_1++;
+    }
+    else
+    {
+      last_pos_1 = 0;
+    }
+    if (last_pos_2 > pos_2)
+    {
+      last_pos_2--;
+    }
+    else
+    {
+      last_pos_2 = NUM_LEDS - 1;
+    }
+    leds[last_pos_1] += CHSV(255, 0, 255);
+    leds[last_pos_2] += CHSV(255, 0, 255);
+  }
+  start_hue += delta_hue;
 }
 
 // =================== Shooting Star Rainbow Mirror ===================
 
+// void shooting_star_rainbow_mirror()
+// {
+
+//   int rate = 2;
+//   int start_hue;
+//   int delta_hue = 30;
+//   start_hue = -1 * millis() / rate;
+//   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star_mirror()); //change fadeby to smaller or larger number to adjust the length of the tail.
+//   uint8_t u;
+//   u = beat8(determine_speed_shooting_star_mirror(), 0); //BPM will allow you to adjust the speed the dot is moving.
+//   // Forward
+//   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
+//   // Backward
+//   uint16_t pos_2 = map(u, 0, 255, NUM_LEDS - 1, 0);
+//   leds[pos_1] = CHSV(start_hue, 255, 255);
+//   leds[pos_2] = CHSV(start_hue, 255, 255);
+//   FastLED.show();
+//   start_hue += delta_hue;
+// }
+
 void shooting_star_rainbow_mirror()
 {
-
-  int rate = 2;
+  int rate = 10;
   int start_hue;
   int delta_hue = 30;
+  static int last_pos_1 = 0;
+  static int last_pos_2 = 0;
   start_hue = -1 * millis() / rate;
-  fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star_mirror()); //change fadeby to smaller or larger number to adjust the length of the tail.
-  uint8_t u;
-  u = beat8(determine_speed_shooting_star_mirror(), 0); //BPM will allow you to adjust the speed the dot is moving.
-  // Forward
+  fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
+  uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
   uint16_t pos_1 = map(u, 0, 255, 0, NUM_LEDS - 1);
-  // Backward
   uint16_t pos_2 = map(u, 0, 255, NUM_LEDS - 1, 0);
-  leds[pos_1] = CHSV(start_hue, 255, 255);
-  leds[pos_2] = CHSV(start_hue, 255, 255);
-  FastLED.show();
+  while (last_pos_1 != pos_1)
+  {
+    if (last_pos_1 < pos_1)
+    {
+      last_pos_1++;
+    }
+    else
+    {
+      last_pos_1 = 0;
+    }
+    if (last_pos_2 > pos_2)
+    {
+      last_pos_2--;
+    }
+    else
+    {
+      last_pos_2 = NUM_LEDS - 1;
+    }
+    leds[last_pos_1] += CHSV(start_hue, 255, 255);
+    leds[last_pos_2] += CHSV(start_hue, 255, 255);
+  }
   start_hue += delta_hue;
 }
 
@@ -438,7 +560,7 @@ void wave_white()
   start_hue = -1 * millis() / rate;
   fadeToBlackBy(leds, NUM_LEDS, determine_length_shooting_star()); //change fadeby to smaller or larger number to adjust the length of the tail.
   uint8_t u = beat8(determine_speed_shooting_star(), 0);           //BPM will allow you to adjust the speed the dot is moving.
-  uint16_t pos = map(u, 0, 255, 0, NUM_LEDS - 1);
+  uint16_t pos = map(u, 0, 255, 0, random(NUM_LEDS - 1));
   while (last_pos != pos)
   {
     if (last_pos < pos)
@@ -448,6 +570,8 @@ void wave_white()
     else
     {
       last_pos = 0;
+      fill_solid(leds, NUM_LEDS, CHSV(0, 0, 0));
+      FastLED.show();
     }
     leds[last_pos] += CHSV(255, 0, 255);
   }
